ef9e30b5a742b59553bdf8494867279cd24154b9
dasimems <isaacseun63@gmail.com>
2023-04-20 16:29:26 -0700

    chores: added some navigation screens

--- assets/lotties/index.js
+++ assets/lotties/index.js
@@ -1,5 +1,7 @@
 import mailLottieFile from "./mail_json.json"
 import SomethingWentWrongLottieFile from "./somethingWentWrong.json"
 import LoadingBars from "./loading_bars.json"
+import LocationPin from "./location.json"
+import LocationPin2 from "./location2.json"
 
-export { mailLottieFile, SomethingWentWrongLottieFile, LoadingBars };
+export { mailLottieFile, LocationPin, LocationPin2, SomethingWentWrongLottieFile, LoadingBars };
